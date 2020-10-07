f96ea9339016ea2b3595d2f8124fc480b53b1fbb
namitmalasi <40159852+namitmalasi@users.noreply.github.com>
2020-10-07 12:53:25 +0530

    Update Power.java
    I have updated the file with indentation and problem statement.

--- src/com/leetcode/problems/Power.java
+++ src/com/leetcode/problems/Power.java
@@ -1,36 +1,52 @@
+/* 
+Implement pow(x, n), which calculates x raised to the power n.
+
+Example 1:
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+
+Example 2:
+
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25  
+*/
+
+
 class Power
-  {
-      public static void main(String args[])
-      {
-        int x=2;
-        int n=-5;
-        System.out.println(pow(x,n));
-      }
-      public static double pow(int x,int n)
-      {
-      double y=1.0;
-      long nn=n;
-      if(n<0)
+    {
+    public static void main(String args[])
         {
-          nn=-1*nn;
+            int x=2;
+            int n=-5;
+            System.out.println(pow(x,n));
         }
-      while(nn>0)
+    public static double pow(int x,int n)
         {
-          if(nn%2==0)
-          {
-            x=x*x;
-            nn=nn/2;
-          }
-          else
-          {
-            y=y*x;
-            nn=nn-1;
-          }
-        }
-        if(n<0)
-        {
-         y=(double)1.0/(double)y;
-        }
-       return y;
-      }
-   }
+            double y=1.0;
+            long nn=n;
+            if(n<0)
+                {
+                    nn=-1*nn;
+                }
+            while(nn>0)
+                {
+                    if(nn%2==0)
+                        {
+                            x=x*x;
+                            nn=nn/2;
+                        }
+                    else
+                        {
+                            y=y*x;
+                            nn=nn-1;
+                        }
+                }
+                    if(n<0)
+                        {
+                            y=(double)1.0/(double)y;
+                        }
+                    return y;
+            }
+    }       
